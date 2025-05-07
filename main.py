fce0a3e57e71e1dd8a36a3bba50e87c8e83c44f3
@Adam0x59 <4dam.5.m@gmail.com>
2025-05-07 18:15:37 +0100

    Added groups to manage update and drawing of sprites

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	pygame.init()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
@@ -23,8 +26,12 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
-		player.draw(screen)
-		player.update(dt)
+		updatable.update(dt)
+		for member in drawable:
+			member.draw(screen)
+		# player.draw(screen)
+		# player.update(dt)
+		# player.move(dt)
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
 
--- player.py
+++ player.py
@@ -28,3 +28,13 @@ class Player(CircleShape):
 			self.rotate(-dt)
 		if keys[pygame.K_d]:
 			self.rotate(dt)
+		self.move(dt)
+
+	def move(self,dt):
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_w]:
+			forward = pygame.Vector2(0, 1).rotate(self.rotation)
+			self.position += forward * PLAYER_SPEED * dt
+		if keys[pygame.K_s]:
+			forward = pygame.Vector2(0, 1).rotate(self.rotation)
+			self.position -= forward * PLAYER_SPEED * dt
