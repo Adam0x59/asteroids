7cdda37bc1f2144c1f23b6a8bfb4b3d2c1bd1a80
@Adam0x59 <4dam.5.m@gmail.com>
2025-05-07 16:22:57 +0100

    Fixed bug in main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main():
 				return
 		screen.fill((0,0,0))
 		player.draw(screen)
-		pygame.display.flip()player.draw(screen)
+		pygame.display.flip()
 		dt = clock.tick(60)
 
 if __name__ == "__main__":
