a00952352b41841b6eff5649dbd1ec620966cdc2
@Adam0x59 <4dam.5.m@gmail.com>
2025-05-07 16:20:44 +0100

    Added a player & draw player to screen

--- main.py
+++ main.py
@@ -2,20 +2,30 @@
 # library to be used throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
+
+	# Initialisation
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+	# Infinite Loop
 	while True:
+
+		# Check if game is still running
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
-		pygame.display.flip()
-			
+		player.draw(screen)
+		pygame.display.flip()player.draw(screen)
+		dt = clock.tick(60)
 
 if __name__ == "__main__":
 	main()
